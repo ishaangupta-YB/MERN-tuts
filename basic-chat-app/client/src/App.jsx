f7de12b9d90aa51596d9887f77f409d37fd4fdae
Ishaan Gupta <ishaangupta999@gmail.com>
2024-04-01 23:55:03 +0530

    New files updated

--- basic-chat-app/client/src/App.jsx
+++ basic-chat-app/client/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react' 
-import './App.css'
-import Form from './modules/Dashboard/Form/Form'
+import "./App.css";
+import Form from "./modules/Dashboard/Form/Form";
 
-import { Routes, Route, Navigate } from 'react-router-dom';
+import {BrowserRouter , Routes, Route  } from "react-router-dom";
+ 
 
 function App() {
-
   return (
-    <div className='bg-[#edf3fc] h-screen flex justify-center items-center'>
-      <Form /> 
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/users/sign_up' element={<Form isSignInPage={false} />} />
+        <Route path='/users/sign_in' element={<Form isSignInPage={true} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
